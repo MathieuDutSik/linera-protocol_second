cd0280881e2a5532c64bff53a765328134ac7473
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-04-15 08:31:44 +0200

    Move file.

--- linera-sdk/src/solidity/linera.sol
+++ linera-sdk/src/solidity/linera.sol
@@ -5,7 +5,8 @@ pragma solidity ^0.8.0;
 // Precompile keys:
 // 0: send_message
 // 1: message_id
-// 2: try_call_application
+// 2: is_bouncing
+// 3: try_call_application
 
 library Linera {
   struct MessageId {
@@ -15,7 +16,9 @@ library Linera {
     uint32 index;
   }
 
-  function send_message(bytes32 chain_id, bytes message) internal pure {
+  enum MessageIsBouncing { NONE, IS_BOUNCING, NOT_BOUNCING }
+
+  function send_message(bytes32 chain_id, bytes message) internal {
     address precompile = address(0x0b);
     bytes1 input1 = bytes1(0);
     bytes memory input2 = abi.encodePacked(input1, chain_id, message);
@@ -24,18 +27,27 @@ library Linera {
     require(output.length == 0);
   }
 
-  function message_id() internal pure returns (MessageId) {
+  function message_id() internal returns (MessageId) {
     address precompile = address(0x0b);
     bytes1 input1 = bytes1(1);
-    (bool success, bytes memory output1) = precompile.call(input2);
+    (bool success, bytes memory output1) = precompile.call(input1);
     require(success);
     MessageId output2 = abi.decode(output1, (MessageId));
     return output2;
   }
 
-  function try_call_application(bytes32 address, bytes memory operation) internal pure returns (bytes) {
+  function message_is_bouncing() internal returns (MessageIsBouncing) {
     address precompile = address(0x0b);
     bytes1 input1 = bytes1(2);
+    (bool success, bytes memory output1) = precompile.call(input1);
+    require(success);
+    MessageIsBouncing output2 = abi.decode(output1, (MessageIsBouncing));
+    return output2;
+  }
+
+  function try_call_application(bytes32 address, bytes memory operation) internal pure returns (bytes) {
+    address precompile = address(0x0b);
+    bytes1 input1 = bytes1(3);
     bytes memory input2 = abi.encodePacked(input1, address, operation);
     (bool success, bytes memory output) = precompile.call(input2);
     require(success);
